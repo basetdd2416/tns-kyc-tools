0b27642dbe647deaf35a93aaf494ee14cafac642
Apisit Onakekasit <blue@appsynth.net>
2018-09-05 17:26:13 +0700

    update crypto

--- src/crypto.js
+++ src/crypto.js
@@ -2,16 +2,17 @@ const crypto = require('crypto');
 
 const algorithm = 'aes-256-ctr';
 const secret = process.env.SECRECT;
+const iv = process.env.IV;
 
 function encrypt(text) {
-  const cipher = crypto.createCipher(algorithm, secret);
+  const cipher = crypto.createCipheriv(algorithm, secret, iv);
   let crypted = cipher.update(text, 'utf8', 'hex');
   crypted += cipher.final('hex');
   return crypted;
 }
 
 function decrypt(text) {
-  const decipher = crypto.createDecipher(algorithm, secret);
+  const decipher = crypto.createDecipheriv(algorithm, secret, iv);
   let dec = decipher.update(text, 'hex', 'utf8');
   dec += decipher.final('utf8');
   return dec;
