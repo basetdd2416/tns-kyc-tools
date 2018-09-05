2dd0856d8e728159a874a9d1206f280995d67864
Apisit Onakekasit <blue@appsynth.net>
2018-09-05 17:55:36 +0700

    add decryption

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('./crypto'); 
 
-const inputDir = path.resolve(__dirname, 'input');
-const outputDir = path.resolve(__dirname, 'output');
+const inputEncryptDir = path.resolve(__dirname, 'encryptions','input');
+const outputEncryptDir = path.resolve(__dirname, 'encryptions','output');
+const inputDecryptDir = path.resolve(__dirname, 'decryptions','input');
+const outDecryptDir = path.resolve(__dirname, 'decryptions','output');
 
 const extFilter = 'gitkeep';
 
@@ -14,18 +16,23 @@ function extension(element) {
   return extName === '.json' 
 };
 
+function extensionD(element) {
+  var extName = path.extname(element);
+  return extName === '.txt' 
+};
 
-fs.readdirSync(inputDir).filter(extension).forEach(file => {
+
+fs.readdirSync(inputEncryptDir).filter(extension).forEach(file => {
   try {
-    const pathToFile = path.resolve(inputDir, file);
+    const pathToFile = path.resolve(inputEncryptDir, file);
     const jsonData = fs.readFileSync(pathToFile, 'utf8')
     const jsObject = JSON.parse(jsonData);
     const obj = JSON.stringify(jsObject);
     const encryptedData = crypto.encrypt(obj);
     const decryptedData = JSON.stringify(JSON.parse(crypto.decrypt(encryptedData)), null ,2)
     const fileNameOutput = path.basename(file, path.extname(file))
-    const pathToOutputEncrypedFile = `${outputDir}/${fileNameOutput}.encrypted `;
-    const pathToOutputDecrypedFile = `${outputDir}/${fileNameOutput}.decrypted `;
+    const pathToOutputEncrypedFile = `${outputEncryptDir}/${fileNameOutput}.encrypted `;
+    const pathToOutputDecrypedFile = `${outputEncryptDir}/${fileNameOutput}.decrypted `;
     fs.writeFileSync(pathToOutputEncrypedFile, encryptedData);
     fs.writeFileSync(pathToOutputDecrypedFile, decryptedData);
     // write file go.
@@ -33,4 +40,19 @@ fs.readdirSync(inputDir).filter(extension).forEach(file => {
     console.log(e);
   }
 });
+
+fs.readdirSync(inputDecryptDir).filter(extensionD).forEach(file => {
+  try {
+    const pathToFile = path.resolve(inputDecryptDir, file);
+    const data = fs.readFileSync(pathToFile, 'utf8')
+    const decryptedData = JSON.stringify(JSON.parse(crypto.decrypt(data)), null ,2)
+    const fileNameOutput = path.basename(file, path.extname(file))
+    const pathToOutputDecrypedFile = `${outDecryptDir}/${fileNameOutput}.decrypted `;
+    fs.writeFileSync(pathToOutputDecrypedFile, decryptedData);
+    // write file go.
+  } catch(e) {
+    console.log(e);
+  }
+});
+
 
